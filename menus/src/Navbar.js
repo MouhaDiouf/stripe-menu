9d93c1fe416406f28c648955aa4cc0e3622ccea6
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-12-01 12:30:59 +0000

    Create content for the NavBar component

--- menus/src/Navbar.js
+++ menus/src/Navbar.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import logo from './images/logo.svg';
+import { useGlobalContext } from './context';
 
 function Navbar() {
-  return <div>Navbar</div>;
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+  return (
+    <nav className="nav">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="stripe" />
+          <button className="btn toggle-btn" onClick={openSidebar}>
+            <FaBars />
+          </button>
+          <ul className="nav-links"></ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
