702ce9a25a0a93df4893eba9a739245c0503e3e6
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-12-01 12:48:13 +0000

    Show the links and sublinks on the sidebar nav

--- menus/src/Navbar.js
+++ menus/src/Navbar.js
@@ -13,7 +13,18 @@ function Navbar() {
           <button className="btn toggle-btn" onClick={openSidebar}>
             <FaBars />
           </button>
-          <ul className="nav-links"></ul>
+          <ul className="nav-links">
+            <li>
+              <button className="link-btn">products</button>
+            </li>
+            <li>
+              <button className="link-btn">developers</button>
+            </li>{' '}
+            <li>
+              <button className="link-btn">company</button>
+            </li>
+          </ul>
+          <button className="btn signin-btn">Sign in</button>
         </div>
       </div>
     </nav>
--- menus/src/Sidebar.js
+++ menus/src/Sidebar.js
@@ -1,7 +1,41 @@
 import React from 'react';
-
+import { FaTimes } from 'react-icons/fa';
+import sublinks from './data';
+import { useGlobalContext } from './context';
 function Sidebar() {
-  return <div>Sidebar</div>;
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+  return (
+    <aside
+      className={`${
+        isSidebarOpen ? 'sidebar-wrapper show' : 'sidebar-wrapper'
+      }`}
+    >
+      <div className="sidebar">
+        <button className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+        <div className="sidebar-links">
+          {sublinks.map((item, index) => {
+            const { links, page } = item;
+            return (
+              <article key={index}>
+                <h4>{page}</h4>
+                {links.map((link, idx) => {
+                  const { url, icon, label } = link;
+                  return (
+                    <a href={url} key={idx}>
+                      {icon}
+                      {label}
+                    </a>
+                  );
+                })}
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </aside>
+  );
 }
 
 export default Sidebar;
